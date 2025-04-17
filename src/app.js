cdbbcb4c75360d18f7372b7f16c83a8142ff60bd
AbhinayThakur <abhinay4203@gmail.com>
2025-04-17 09:34:39 +0530

    Add helmet middleware for security and enforce HTTPS in app.js; update package.json and package-lock.json to include helmet dependency

--- src/app.js
+++ src/app.js
@@ -2,16 +2,25 @@ import dotenv from "dotenv";
 
 dotenv.config({
     path: "./.env"
-})
+});
 
 import express from 'express';
 import cors from 'cors';
+import helmet from "helmet";
 import cookieParser from 'cookie-parser';
-
 
 const app = express();
 
 const allowedOrigins = process.env.CORS_ORIGIN.split(',');
+
+app.use(helmet()); // Security middleware to set various HTTP headers
+// Middleware to enforce HTTPS
+app.use((req, res, next) => {
+    if (req.headers["x-forwarded-proto"] !== "https" && process.env.NODE_ENV === "production") {
+        return res.redirect(`https://${req.headers.host}${req.url}`);
+    }
+    next();
+});
 
 app.use(cors({
     origin: (origin, callback) => {
@@ -25,23 +34,19 @@ app.use(cors({
     credentials: true,
 }));
 
-// console.log("ENV TEST - CLOUDINARY API KEY:", process.env.CLOUDINARY_API_KEY || "Not Loaded");
-
-
-app.use(express.json({limit: "16kb"}));
-app.use(express.urlencoded({extended: true,limit: "16kb"}));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
 // Routes
-import adminRouters from  "../src/routes/admin.routes.js";
+import adminRouters from "../src/routes/admin.routes.js";
 import eventsRouters from '../src/routes/event.routes.js';
 import facultyRouters from '../src/routes/faculty.routes.js';
 import galleryRouters from '../src/routes/gallery.routes.js';
 import feedbackRouters from '../src/routes/feedback.routes.js';
 import newsletterRouters from '../src/routes/newsletter.routes.js';
 import registerRouters from '../src/routes/register.routes.js';
-
 
 app.use('/api/v1/admin', adminRouters);
 app.use('/api/v1/Events', eventsRouters);
@@ -51,4 +56,4 @@ app.use('/api/v1/Feedback', feedbackRouters);
 app.use('/api/v1/Newsletter', newsletterRouters);
 app.use('/api/v1/Register', registerRouters);
 
-export  {app }
+export { app };
