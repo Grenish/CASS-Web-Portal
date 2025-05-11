22d2a5f13d1aeddd39eb1b27a000d72417275ef5
Grenish rai <107925840+Grenish@users.noreply.github.com>
2025-05-11 21:39:05 +0530

    Refactor index.js for improved readability: standardize formatting and indentation in dotenv configuration and database connection handling.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,19 @@
 import dotenv from "dotenv";
 
 dotenv.config({
-    path: "./.env"
-})
-
+  path: "./.env",
+});
 
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
 
-
-
 connectDB()
-    .then(
-        () => {
-            app.listen(process.env.PORT || 5000, () => {
-                console.log(`⚙️ Server is running on port ${process.env.PORT}!`);
-            })
-           
-        })
-    .catch( (err) => {
-        console.log("MONGODD CONNECTION FAILED !!!",err);
-        process.exit(1)
-    })
+  .then(() => {
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`⚙️ Server is running on port ${process.env.PORT}!`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODD CONNECTION FAILED !!!", err);
+    process.exit(1);
+  });
